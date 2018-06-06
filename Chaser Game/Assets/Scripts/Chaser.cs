fa684d8a352de1b49833f987c44e80f65244f424
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-06 16:24:43 +0300

    Add scripts, materials, etc.

---
+++ Chaser Game/Assets/Scripts/Chaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour {
+
+    public Transform targetTransform; // Reference to the Player object transform. Usable in Unity Inspector by assining the Player transform to this variable.
+    public float speed = 7f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 displacementFromTarget = targetTransform.position - transform.position; // Calculating the displacement between the Player and our own object (Chaser).
+        Vector3 directionToTarget = displacementFromTarget.normalized; // Getting the direction towards the target
+        Vector3 velocity = directionToTarget * speed; // Velocity -> number of meters traveled in a given direction per second
+        Vector3 moveAmount = velocity * Time.deltaTime; // Move amount -> velocity * Time.deltaTime
+
+        float distanceToTarget = displacementFromTarget.magnitude; // Set a distance from the target. Magnitude -> the distance between the vector's origin (0,0,0) and its endpoint
+
+        if(distanceToTarget > 1.5f) { // Only if distance is greater than 1.5 then move the objet towards the target.
+            transform.Translate(moveAmount); // Moving the player.
+        }
+        
+    }
+}
---
+++ Chaser Game/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10; // Speed -> number of meters traveled per second.
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); // Creating Vector3 struct and getting the X and Y axis input
+        Vector3 direction = input.normalized; // To get the direction we need to normalize it. When normalized, a vector keeps the same direction but its length is 1.0
+        Vector3 velocity = direction * speed; // Velocity -> number of meters traveled in a given direction per second
+        Vector3 moveAmount = velocity * Time.deltaTime; // Move amount -> velocity * Time.deltaTime
+
+        //transform.position += moveAmount; // Moving the player. Current position = current position + moveAmount
+        transform.Translate(moveAmount); // Alternative way of doing the movement. More common.
+    }
+}
